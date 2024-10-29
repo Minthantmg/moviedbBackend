c75788c6687e7e89e7b70c2d23aff7c45d69c57c
MinThant <minthant180@gmail.com>
2024-10-29 13:35:16 +0900

    feat:create getMovieById rpute

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -9,17 +9,32 @@ const createMovie = async (req, res) => {
   }
 };
 
-const getAllMovies = async (req,res) => {
-  try{
+const getAllMovies = async (req, res) => {
+  try {
     const movies = await Movie.find();
-    res.status(200).json(movies)
-  }catch(error){
-    res.status(500).json({message: error.message})
+    if (movies.length === 0) {
+      return res.status(404).json({ message: "No movies found" });
+    }
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-}
+};
 
+const getMovieById = async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    if (!movie) {
+      return res.status(404).json({ message: "Movie not found" });
+    }
+    res.status(200).json(movie);
+  } catch {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = {
-    createMovie,
-    getAllMovies
-  };
+  createMovie,
+  getAllMovies,
+  getMovieById,
+};
--- routes/movie.route.js
+++ routes/movie.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const {createMovie, getAllMovies} = require("../controllers/movie.controller")
+const {createMovie, getAllMovies, getMovieById} = require("../controllers/movie.controller")
 
 router.post("/movie",createMovie)
 router.get("/movies",getAllMovies)
+router.get("/movie/:id",getMovieById)
 
 module.exports = router;
