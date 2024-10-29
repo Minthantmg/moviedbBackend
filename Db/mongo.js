e7311b618f42fbfea2345f31e138fa2e720b82e0
MinThant <minthant180@gmail.com>
2024-10-29 11:13:37 +0900

    fix:change log message to more understandable

--- Db/mongo.js
+++ Db/mongo.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
 dotenv.config();
 
 const connectToDatabase = async () => {
   try {
     const dbUrl = process.env.MONGO_URL;
+    if(!dbUrl){
+      throw new Error("MongoDb connection failed!Please check env file for enviroment variable!")
+    }
     mongoose.set("strictQuery", false);
     await mongoose.connect(dbUrl);
-    console.log("connected!");
+    console.log("Database connection successful!");
   } catch (error) {
-    console.log(error);
+    console.error("Database connection error:", error.message);
+    process.exit(1);
   }
 };
 
