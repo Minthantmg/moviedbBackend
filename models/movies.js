3beb0b4f0065d540caf863b76d155901b2b5c710
MinThant <minthant180@gmail.com>
2024-10-29 14:31:42 +0900

    feat:add comment section

--- models/movies.js
+++ models/movies.js
@@ -1,5 +1,26 @@
 const mongoose = require("mongoose");
 
+const CommentSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  commentText: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 10,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
 const MovieSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -36,6 +57,7 @@ const MovieSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  comments: [CommentSchema],
 });
 
 const Movie = mongoose.model("Movie", MovieSchema);
