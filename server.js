be8b8d233d5cc1b4500727c5d3a4a5070eea8e97
MinThant <minthant180@gmail.com>
2024-11-01 17:16:34 +0900

    feat:apply user route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/mongo");
 const corsMiddleware = require("./middlewares/cors");
 const movieRouter = require("./routes/movie.route");
+const userRouter = require("./routes/user.route");
 const port = 4000;
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(corsMiddleware);
 
 app.use("/api/v1", movieRouter);
+app.use("/api/v1", userRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json("welcome");
