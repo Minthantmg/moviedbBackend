e5248f5eb2dcdde95e1c0dec31f78740ee7c2bb2
Min Thant Mg <minthant180@gmail.com>
2024-11-02 23:51:41 +0900

    feat:create login route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const {createUser,getAllUsers} = require("../controllers/user.controller")
+const {createUser,getAllUsers,login} = require("../controllers/user.controller")
 
 router.post("/register",createUser)
 router.get("/users",getAllUsers)
+router.post("/login",login)
 
 module.exports = router;
