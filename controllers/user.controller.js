8816744e39bb69a2241aa221982688f5de3a6ee3
Min Thant Mg <minthant180@gmail.com>
2024-11-03 23:27:50 +0900

    fix:add login logic

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,12 +19,23 @@ const getAllUsers = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    try{
-        console.log("entered")
-    }catch(error){
-        res.status(500).json({message: error.message});
+    try {
+        console.log(req.body);
+
+        const user = await User.findOne({ email: req.body.email });
+
+        // if (!user) {
+        //     return res.status(404).json({ message: "User not found" });
+        // }
+
+        res.status(200).json(user);
+        console.log("User found:", user);
+    } catch (error) {
+        console.error("Error occurred during login:", error);
+        res.status(500).json({ message: error.message });
     }
-}
+};
+
 
 module.exports = {
     createUser,
