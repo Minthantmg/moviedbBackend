1ddf3a66b6e852c58fd49483e6575c4987d9f957
MinThant <minthant180@gmail.com>
2024-11-05 11:35:17 +0900

    fix:create login route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {createUser,getAllUsers,login} = require("../controllers/user.controller")
+const {createUser,getAllUsers,login,getUserById} = require("../controllers/user.controller")
 
 router.post("/register",createUser)
 router.get("/users",getAllUsers)
 router.post("/login",login)
+router.get("/user/:id",getUserById)
 
 module.exports = router;
