c23ecbf5622ef8ece8ec69daf5d5c24312d357b8
MinThant <minthant180@gmail.com>
2024-11-05 11:46:20 +0900

    feat:added delete route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {createUser,getAllUsers,login,getUserById} = require("../controllers/user.controller")
+const {createUser,getAllUsers,login,getUserById, deleteUserById} = require("../controllers/user.controller")
 
 router.post("/register",createUser)
 router.get("/users",getAllUsers)
 router.post("/login",login)
 router.get("/user/:id",getUserById)
+router.delete("/user/:id",deleteUserById)
 
 module.exports = router;
