5a1817e182c06900ff77aa37b297d352d55cf5f4
MinThantMaung <minthant180@gmail.com>
2024-11-11 09:46:19 +0900

    feat:update update route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {createUser,getAllUsers,login,getUserById, deleteUserById} = require("../controllers/user.controller")
+const {createUser,getAllUsers,login,getUserById, deleteUserById, updateUserById} = require("../controllers/user.controller")
 
 router.post("/register",createUser)
 router.get("/users",getAllUsers)
 router.post("/login",login)
 router.get("/user/:id",getUserById)
 router.delete("/user/:id",deleteUserById)
+router.put("/user/:id",updateUserById)
 
 module.exports = router;
